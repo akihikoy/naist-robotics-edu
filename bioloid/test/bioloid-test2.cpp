e5fb2bf6b3b96062115a7782be2c9b118279d27a
Akihiko Yamaguchi <->
2012-10-18 16:31:33 +0900

    Added new bioloid test code (bioloid-test2.cpp);

---
+++ bioloid/test/bioloid-test2.cpp
@@ -0,0 +1,157 @@
+//-------------------------------------------------------------------------------------------
+/*! \file    bioloid-test2.cpp
+    \brief   bioloid test program 2
+    \author  Akihiko Yamaguchi
+    \version 0.1
+    \date    Aug.02, 2012
+*/
+//-------------------------------------------------------------------------------------------
+#include <bioloid.h>
+#include <fstream>
+//-------------------------------------------------------------------------------------------
+namespace loco_rabbits
+{
+
+// inline double RadToDeg(const double &rad)
+// {
+  // return rad/M_PI*180.0;
+// }
+
+template <typename t_angle_fwditr>
+inline void ConstrainAngles (t_angle_fwditr begin, t_angle_fwditr end)
+{
+  // const double angle_max[18]={ 0.0, 1.6, 1.6, 1.6, 1.6, 1.5,   1.57, 1.57, 1.6, 1.6, 1.6, 1.5,   1.6, 0.0, 1.6, 1.6, 1.6, 1.5};
+  // const double angle_min[18]={-1.6,-0.0,-1.6,-1.6,-1.5,-1.6,  -1.57,-1.57,-1.6,-1.6,-1.5,-1.6,  -0.0,-1.6,-1.6,-1.6,-1.5,-1.6};
+  const double angle_max[18]={  0,90, 90, 90, 90, 80,   90, 90, 90, 90, 90, 80,   90,  0, 90, 90, 90, 80};
+  const double angle_min[18]={-90,-0,-90,-90,-80,-90,  -90,-90,-90,-90,-80,-90,   -0,-90,-90,-90,-80,-90};
+  const double *amax(angle_max), *amin(angle_min);
+  for (; begin!=end; ++begin,++amax,++amin)
+  {
+    if(*begin>*amax)  *begin= *amax;
+    else if (*begin<*amin)  *begin= *amin;
+  }
+}
+
+}
+//-------------------------------------------------------------------------------------------
+using namespace std;
+using namespace loco_rabbits;
+using namespace serial;
+using namespace bioloid;
+//-------------------------------------------------------------------------------------------
+// #define print(var) print_container((var), #var"= ")
+// #define print(var) std::cout<<#var"= "<<(var)<<std::endl
+//-------------------------------------------------------------------------------------------
+
+int main(int argc, char**argv)
+{
+  TBioloidController  bioloid;
+
+  // bioloid.Connect("/dev/ttyUSB0");
+  // bioloid.TossMode();
+  // // bioloid.TossTest();
+
+  bioloid.ConnectBS("/dev/ttyUSB0",1);
+
+usleep(500000);
+
+
+//   bioloid.SetLightDetectCompare (100,128);
+
+  bioloid.LED(1,1);
+// usleep(50000);
+  bioloid.LED(2,1);
+// usleep(50000);
+  bioloid.LED(3,1);
+// usleep(50000);
+  bioloid.LED(4,1);
+// usleep(50000);
+  bioloid.LED(5,1);
+// usleep(50000);
+
+  bioloid.LED(3,0);
+// usleep(50000);
+
+  for(int i(1);i<=20;++i)
+  {
+    if(bioloid.Ping(i)==0)  cerr<<"Dynamixel found: "<<i<<endl;
+    else  cerr<<"no Dynamixel: "<<i<<endl;
+// usleep(50000);
+  }
+  for(int i(1);i<=20;++i)
+  {
+    bioloid.LED(3,i%2);
+usleep(50000);
+  }
+// return 0;
+
+  // int ids[]= {1,2};
+  // int ids[]= {2,3};
+  int ids[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+  double angles[SIZE_OF_ARRAY(ids)], angles_observed[SIZE_OF_ARRAY(ids)];
+
+  ofstream ofs_goal("goal.dat");
+  ofstream ofs_angle("angle.dat");
+  int skip(0);
+  double omega(2.0*M_PI*1), goal, distance_c;
+  double time_offset(GetCurrentTime()), time(GetCurrentTime()), t;
+
+  #if 0
+  while (time-time_offset<2.0)
+  {
+    bioloid.GetAllAngles (ids,ids+SIZE_OF_ARRAY(ids), angles_observed);
+    bioloid.GoTo (ids,ids+SIZE_OF_ARRAY(ids), angles);
+    usleep(10000);
+    time= GetCurrentTime();
+  }
+  #endif
+  LMESSAGE("----");
+
+  while (time-time_offset<60.0)
+  {
+    t= time-time_offset;
+    // goal= 150.0*sin(omega*t);
+    goal= 30.0*sin(omega*t);
+    LMESSAGE(t<<"[s]: ("<<goal<<") ");
+
+    #if 1
+    // bioloid.GetAngle(1);
+    if(skip==0)
+    {
+      bioloid.GetAllAngles (ids,ids+SIZE_OF_ARRAY(ids), angles_observed);
+      ofs_angle<<t<<" "<<PrintVector(angles_observed,angles_observed+SIZE_OF_ARRAY(angles_observed))<<endl;
+      // bioloid.GetAllAngles (ids,ids+5, angles_observed);
+      // ofs_angle<<t<<" "<<PrintVector(angles_observed,angles_observed+5)<<endl;
+      skip=0;
+    } else{--skip;}
+    std::fill (angles,angles+SIZE_OF_ARRAY(angles),goal);
+//     ConstrainAngles (angles,angles+SIZE_OF_ARRAY(angles));
+    bioloid.GoTo (ids,ids+SIZE_OF_ARRAY(ids), angles);
+// usleep(6000);
+// usleep(10000);
+    ofs_goal<<t<<" "<<PrintVector(angles,angles+SIZE_OF_ARRAY(angles))<<endl;
+    #endif
+
+    #if 0
+    bioloid.GetDistance(100,0,distance_c);
+    ofs_angle<<t<<" "<<distance_c<<endl;
+    #endif
+
+    // usleep(5000);
+    // usleep(10000);
+    // usleep(15000);
+    // usleep(20000);
+    // usleep(30000);
+    // usleep(50000);
+    // usleep(100000);
+    time= GetCurrentTime();
+  }
+
+  ofs_goal.close();
+  ofs_angle.close();
+
+  bioloid.Disconnect();
+
+  return 0;
+}
+//-------------------------------------------------------------------------------------------
